bef25a4df795f8cdcec3d14d2c74ce900f306c10
[Muhammad <[abdullahzaman581@gmail.com]>
2024-12-24 01:27:55 +0500

    3 question done

---
+++ chapter 35 to 38/app.js
@@ -0,0 +1,54 @@
+                            // **************************** Question 1 **************************
+
+
+
+
+// function currentDate(){
+//     now = new Date();
+//     return now;
+// }
+
+
+// console.log(currentDate());
+
+
+
+
+
+
+                            // **************************** Question 2 **************************
+
+// function fullName(){
+//     var fname =  prompt("enter your first name");
+//     var lname =  prompt("enter your last name");
+//     return "Hello" + " " + fname + " " + lname 
+// }
+
+// console.log(fullName());
+
+
+
+
+
+                            // **************************** Question 3 **************************
+
+
+// function sum(){
+//     var input1 = +prompt("enter your first number");
+//     var input2 = +prompt("enter your second number");
+//     return input1 + input2
+// }
+
+// console.log(sum());
+
+
+
+
+                            // **************************** Question 4 **************************
+
+
+
+
+
+
+
